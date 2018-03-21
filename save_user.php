531f7b672b94a8e4c1ebec462865a87af2dad7db
chrisssme <37619435+chrisssme@users.noreply.github.com>
2018-03-21 17:42:47 +0300

    Файлы для сайта

---
+++ save_user.php
@@ -0,0 +1,46 @@
+﻿<body background="11.jpg">
+<?php
+if (isset($_POST['login'])) { $login = $_POST['login']; if ($login == '') { unset($login);} } //заносим введенный пользователем логин в переменную $login, если он пустой, то уничтожаем переменную
+if (isset($_POST['password'])) { $password=$_POST['password']; if ($password =='') { unset($password);} }
+//заносим введенный пользователем пароль в переменную $password, если он пустой, то уничтожаем переменную
+
+if (empty($login) or empty($password)) //если пользователь не ввел логин или пароль, то выдаем ошибку и останавливаем скрипт
+{
+exit ("Вы ввели не всю информацию! <a href='index.php'>Вернуться назад и заполнить все поля</a>");
+}
+//если логин и пароль введены,то обрабатываем их, чтобы теги и скрипты не работали, мало ли что люди могут ввести
+$login = stripslashes($login);
+$login = htmlspecialchars($login);
+
+$password = stripslashes($password);
+$password = htmlspecialchars($password);
+
+//удаляем лишние пробелы
+$login = trim($login);
+$password = trim($password);
+
+// подключаемся к базе
+include ("bd.php");// файл bd.php должен быть в той же папке, что и все остальные, если это не так, то просто измените путь 
+
+// проверка на существование пользователя с таким же логином
+
+$result = mysqli_query($db, "SELECT username FROM users WHERE username='".$login."'");
+
+$myrow = mysqli_fetch_array($result);
+
+if (!empty($myrow['username'])) {
+exit ("Извините, введённый Вами логин уже зарегистрирован. Введите другой логин. <a href='index.php'>Главная страница</a>");
+}
+
+// если такого нет, то сохраняем данные
+$result2 = mysqli_query ($db, "INSERT INTO users (username,password) VALUES('$login','$password')");
+// Проверяем, есть ли ошибки
+if ($result2=='TRUE')
+{
+echo "Вы успешно зарегистрированы! Теперь вы можете зайти на сайт. <a href='index.php'>Главная страница</a>";
+}
+
+else {
+echo "Ошибка! Вы не зарегистрированы.";
+     }
+?>
